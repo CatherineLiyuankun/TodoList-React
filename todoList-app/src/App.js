d569212ca35728ba9c1a64c79c3b7fa038c5af92
Li,Yuankun <yuanli@microstrategy.com>
2016-12-16 22:05:51 +0800

    modify css, need to add importance event and tab bar

--- todoList-app/src/App.js
+++ todoList-app/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import logo from './logo.svg';
-import './App.css';
+import logo from './images/logo.svg';
+import Model from './Model.js';
+import './css/App.css';
 
 class App extends Component {
   render() {
@@ -21,9 +22,9 @@ var TodoListApp = React.createClass({
   getInitialState: function() {
     return {
       data: [
-        {"id": "0001", "task":"test", "complete": false},
-        {"id": "0002", "task":"investigate issue", "complete": false},
-        {"id": "0003", "task":"fix issue", "complete": true},
+        {"id": "0001", "task":"test", "importance":"Normal", "complete": false},
+        {"id": "0002", "task":"investigate issue", "importance":"Normal", "complete": false},
+        {"id": "0003", "task":"fix issue", "importance":"Normal", "complete": true},
       ]
     };
   },
@@ -43,8 +44,8 @@ var TodoListApp = React.createClass({
 
   handleTaskAdd: function(taskContent) {
     var data = this.state.data;
-    var newItem = [{"id": this.generateId(), "task": taskContent, "complete": false}];
-    data = data.concat(newItem);
+    var newItem = [{"id": this.generateId(), "task": taskContent, "importance":"Normal", "complete": false}];
+    data = newItem.concat(data);
     this.setState({data});
   },
 
@@ -70,6 +71,10 @@ var TodoListApp = React.createClass({
     this.setState({data});
   },
 
+  handleTaskIndex: function(taskId){
+    return this.getIndexById(taskId, this.state.data);
+  },
+
   render: function() {
     var statistics = {
       // count the total task number and completed task number
@@ -84,8 +89,9 @@ var TodoListApp = React.createClass({
         <TodoAction addTask={this.handleTaskAdd}/>
         <TodoDataGrid data={this.state.data}
             deleteTask={this.handleTaskDelete}
-            toggleComplete={this.handleToggleComplete}
+            toggleComplete={this.handleToggleComplete} 
             updateTask={this.handleUpdateTask}
+            getTaskIndex={this.handleTaskIndex}
              />
         <TodoFooter todoCount={statistics.todoCount}
             todoCompleteCount={statistics.todoCompleteCount} />
@@ -99,8 +105,8 @@ var TodoHeader = React.createClass({
   render: function() {
     return (
       <div className="Todo-header">
-      <img src={logo} className="App-logo" alt="logo" />
-      <h2>{this.props.headerName}</h2>
+        <div className="logo-container"><img src={logo} className="App-logo" alt="logo" /></div>
+        <div className="header-text-container"><h2 className="App-header-text">{this.props.headerName}</h2></div>
       </div>
     )   
   }
@@ -136,10 +142,12 @@ var TodoDataGrid = React.createClass({
           taskId={listItem.id}
           key={listItem.id}
           task={listItem.task}
+          importance={listItem.importance}
           complete={listItem.complete}
           deleteTask={this.props.deleteTask}
           updateTask={this.props.updateTask}
-          toggleComplete={this.props.toggleComplete} />
+          toggleComplete={this.props.toggleComplete}
+          getTaskIndex={this.props.getTaskIndex}/>
       )
     }, this);
 
@@ -150,9 +158,9 @@ var TodoDataGrid = React.createClass({
         </div>
         <div className="DataGrid-itemsScrollBox">
             <div className="DataGrid-itemsContainer">
-                  <table className="DataGrid-headerTable" cellSpacing="0" cellPadding="0">
+                  <table className="DataGrid-itemwrap-table" cellSpacing="0" cellPadding="0">
                     <colgroup>
-                      <col className="col0" /><col className="col1" /><col className="col2" />
+                      <col className="col0" /><col className="col1" /><col className="col2" /><col className="col3" />
                     </colgroup>
                     <tbody>
                        {itemsList}
@@ -167,16 +175,16 @@ var TodoDataGrid = React.createClass({
 
 var TodoGridHeader = React.createClass({
   render: function() {
-    var gridheaderNames = ['', 'Sort By Task', ''];
+    var gridheaderNames = ['', 'Sort By Task', 'Sort By importance'];
     var gridheader = gridheaderNames.map(function(headerColumn) {
       return (
-        <td>{headerColumn}</td>
+        <td><div className="DataGrid-headerText">{headerColumn}</div></td>
       )
     });
     return (
       <table className="DataGrid-headerTable" cellSpacing="0" cellPadding="0">
         <colgroup>
-          <col className="col0" /><col className="col1" /><col className="col2" />
+          <col className="col0-h" /><col className="col1-h" /><col className="col2-h" /><col className="col3-h" />
         </colgroup>
         <tbody>
           <tr>{gridheader}</tr>
@@ -187,6 +195,15 @@ var TodoGridHeader = React.createClass({
 });
 
 var TodoRow = React.createClass({
+  getInitialState: function() {
+    return {
+      classes: "DataRow",
+      index: 0,
+    };
+  },
+  componentDidMount: function() {
+    this.setState({classes: this.state.classes, index: this.getTaskIndex()});
+  },
   toggleComplete: function() {
     this.props.toggleComplete(this.props.taskId);
   },
@@ -197,15 +214,31 @@ var TodoRow = React.createClass({
   deleteTask: function() {
     this.props.deleteTask(this.props.taskId);
   },
-
+  getTaskIndex: function() {
+    return this.props.getTaskIndex(this.props.taskId);
+  },
+  selectTask: function() {
+    var classes = this.state.classes;
+    classes += " selected";
+    // this.setState({classes: classes, index: this.getTaskIndex()});
+  },
+  importanceTask: function(){
+    
+  },
   render: function() {
+    var classes = this.state.classes;
+    classes += (this.getTaskIndex() % 2 === 1) ? ' odd' : ' even';
+
     return (
-      <tr>
+      <tr className={classes} onFocus={this.selectTask} >
         <td>
           <input type="checkbox" checked={this.props.complete} onChange={this.toggleComplete} className="check-task" />
         </td>
         <td>
-           <input className="update-task" ref="taskContent" onChange={this.updateTask} value={this.props.task} />
+           <input className="task-content" ref="taskContent" onChange={this.updateTask} value={this.props.task} />
+        </td>
+        <td>
+           <button className="importance-button" ref="taskImportance" onChange={this.updateTask} onClick={this.importanceTask}>{this.props.importance}</button>
         </td>
         <td>
           <div className="delete-task" onClick={this.deleteTask}></div>
@@ -218,7 +251,7 @@ var TodoRow = React.createClass({
 var TodoFooter = React.createClass({
   render: function() {
     return (
-      <li className="list-group-item"> total: {this.props.todoCount} actives: {this.props.todoCount - this.props.todoCompleteCount} completes: {this.props.todoCompleteCount}</li>
+      <div className="Todo-footer"> total: {this.props.todoCount} actives: {this.props.todoCount - this.props.todoCompleteCount} completes: {this.props.todoCompleteCount}</div>
     )
   }
 });
---
+++ todoList-app/src/Model.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+var Model = React.createClass({
+	// generate a random id for new task 
+  generateId: function() {
+    return Math.floor(Math.random() * 9000) + 1000;
+  },
+
+  getIndexById: function (taskId, data) {
+    for ( var index = 0; index < data.length; index++) {
+        if (data[index].id === taskId)
+            return index;
+    }
+  },
+  render() {
+    return (
+      <div className="model">
+      </div>
+    );
+  }
+});
